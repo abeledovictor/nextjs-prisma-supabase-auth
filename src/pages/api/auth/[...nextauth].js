81c8c2717bb45b9fae80b5ce5b2bc974e0f3d63a
vabeledo <abeledovictor@gmail.com>
2023-02-01 20:35:56 +0100

    add welcome email

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -9,7 +9,35 @@ import { PrismaClient } from '@prisma/client';
 // Instantiate Prisma Client
 const prisma = new PrismaClient();
 
+const transporter = nodemailer.createTransport({
+  host: process.env.EMAIL_SERVER_HOST,
+  port: process.env.EMAIL_SERVER_PORT,
+  auth: {
+    user: process.env.EMAIL_SERVER_USER,
+    pass: process.env.EMAIL_SERVER_PASSWORD,
+  },
+  secure: true,
+});
+
+const sendWelcomeEmail = async ({ user }) => {
+  const { email } = user;
+
+  try {
+    await transporter.sendMail({
+      from: `"✨ APP - " ${process.env.EMAIL_FROM}`,
+      to: email,
+      subject: 'Welcome! 🎉',
+      html: '<div><p>Welcome!</p><p>I hope you enjoy using my app.</p></div>',
+    });
+  } catch (error) {
+    console.log(`❌ Unable to send welcome email to user (${email})`);
+  }
+};
+
 export default NextAuth({
+  events: {
+    createUser: sendWelcomeEmail
+  },
   adapter: PrismaAdapter(prisma),
     providers: [
       GoogleProvider({
