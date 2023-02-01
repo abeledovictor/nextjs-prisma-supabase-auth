0450468732d241a755b1e680754cab6c22e870cb
vabeledo <abeledovictor@gmail.com>
2023-02-01 20:00:10 +0100

    add google provider

--- README.md
+++ README.md
@@ -4,7 +4,11 @@ This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next
 
 First, create a new project at supabase.co
 
-Second, edit .env variables check .env.example for inspiration.
+Second, edit .env variables check .env.example for inspiration. You will need to also setup Google OAuth ( https://console.cloud.google.com/ )
+
+On Google Auth console add http://localhost:3000/api/auth/callback/google as redirect URI
+
+If you don't want Google auth you can disable it at src/pages/api/auth/[...nextauth].js by commenting out GoogleProvider
 
 After that run
 
--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -2,6 +2,7 @@
 import NextAuth from 'next-auth';
 import EmailProvider from 'next-auth/providers/email';
 import nodemailer from 'nodemailer';
+import GoogleProvider from 'next-auth/providers/google';
 import { PrismaAdapter } from '@next-auth/prisma-adapter';
 import { PrismaClient } from '@prisma/client';
 
@@ -11,6 +12,10 @@ const prisma = new PrismaClient();
 export default NextAuth({
   adapter: PrismaAdapter(prisma),
     providers: [
+      GoogleProvider({
+        clientId: process.env.GOOGLE_ID,
+        clientSecret: process.env.GOOGLE_SECRET,
+      }),
       EmailProvider({
         server: {
           host: process.env.EMAIL_SERVER_HOST,
