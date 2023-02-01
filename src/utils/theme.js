fc878c303e3ff7749bf18dcbebf21a2a780bd97a
vabeledo <abeledovictor@gmail.com>
2023-02-01 19:29:12 +0100

    add material ui setup

---
+++ src/utils/theme.js
@@ -0,0 +1,18 @@
+import { createTheme } from "@mui/material";
+
+ export const theme = createTheme({
+    palette: {
+        primary: {
+            light: '#ff9f76',
+            main: '#ff6d49',
+            dark: '#c63b1e',
+            contrastText: '#fff',
+        },
+        secondary: {
+            light: '#ffdc9c',
+            main: '#ffaa6d',
+            dark: '#c87b40',
+            contrastText: '#fff',
+        },
+    },
+});
