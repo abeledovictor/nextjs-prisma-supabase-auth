9c43ceaa4c2baa0112c26f9e47db3c582c3efa19
vabeledo <abeledovictor@gmail.com>
2023-02-01 19:14:07 +0100

    Add Email provider auth example

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,6 @@
+import { SessionProvider as AuthProvider } from 'next-auth/react';
 import '@/styles/globals.css'
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+export default function App({ Component, session, ...pageProps }) {
+  return <AuthProvider session={session}><Component {...pageProps} /></AuthProvider>
 }
---
+++ src/pages/api/auth/[...nextauth].js
@@ -0,0 +1,27 @@
+// pages/api/auth/[...nextauth].js
+import NextAuth from 'next-auth';
+import EmailProvider from 'next-auth/providers/email';
+import nodemailer from 'nodemailer';
+import { PrismaAdapter } from '@next-auth/prisma-adapter';
+import { PrismaClient } from '@prisma/client';
+
+// Instantiate Prisma Client
+const prisma = new PrismaClient();
+
+export default NextAuth({
+  adapter: PrismaAdapter(prisma),
+    providers: [
+      EmailProvider({
+        server: {
+          host: process.env.EMAIL_SERVER_HOST,
+          port: process.env.EMAIL_SERVER_PORT,
+          auth: {
+            user: process.env.EMAIL_SERVER_USER,
+            pass: process.env.EMAIL_SERVER_PASSWORD,
+          },
+        },
+        from: process.env.EMAIL_FROM,
+        maxAge: 15 * 60, // Magic links are valid for 15 min only
+      }),
+    ],
+  });
