a46f15aedc077aa63c331dd3fc333d34c7f7c2b7
vabeledo <abeledovictor@gmail.com>
2023-02-02 15:37:13 +0100

    update redirectTo for protectedRoute

--- src/utils/protectedRoute.js
+++ src/utils/protectedRoute.js
@@ -1,15 +1,19 @@
 import { AUTH_OPTIONS } from '@/pages/api/auth/[...nextauth]'
 import { getServerSession } from "next-auth/next"
 
+// Using auto generated default value
+// For more info about pages check https://next-auth.js.org/configuration/options#pages
+const NEXT_AUTH_DEFAULT_URL = '/api/auth/signin'
 
-export function protectedRoute(redirectUrl = '/') {
+export function protectedRoute(redirectUrl) {
+    const redirectTo = redirectUrl || NEXT_AUTH_DEFAULT_URL
     return async (context) => {
         const session = await getServerSession(context.req, context.res, AUTH_OPTIONS)
 
         if (!session) {
           return {
             redirect: {
-              destination: redirectUrl,
+              destination: redirectTo,
               permanent: false,
             },
           }
